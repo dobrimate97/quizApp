21cc929c7f928ea7d559233e39d1710cf7e02f66
matedobri97 <dobrics89@gmail.com>
2023-06-27 10:36:29 +0200

    First commit

---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Dobri Máté on 2023. 06. 27..
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
